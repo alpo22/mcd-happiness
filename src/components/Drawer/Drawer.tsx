e32e65b8c1f4a8aaf2080725caca92cefbe68767
alpo22 <alan.s.powell@gmail.com>
2024-02-01 09:36:18 -0800

    Use different drawer. Fix typescript.

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,40 +1,34 @@
-import RMDrawer from "react-modern-drawer";
+import { Drawer as AntDrawer } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
-import "react-modern-drawer/dist/index.css";
 import "./Drawer.scss";
 
-function Drawer({ countryCodeMappings }) {
+interface IDrawer {
+  countryCodeMappings: Map<string, string>;
+}
+
+function Drawer({ countryCodeMappings }: IDrawer) {
   const navigate = useNavigate();
   const params = useParams();
 
-  const countryFullName = countryCodeMappings.get(params.countryCode);
+  const countryFullName = countryCodeMappings.get(params.countryCode!);
   const title = countryFullName ? `Happiness in ${countryFullName}` : "Unknown country";
 
-  // TODO: typescript
-  // TODO: wider
-  // TODO: close button
   // TODO: show all their ratings in a graph
-  // TODO: show their global rankings?
 
   return (
-    <RMDrawer
-      className="drawer"
+    <AntDrawer
       open
       onClose={() => {
         navigate("/");
       }}
-      direction="right"
+      title={title}
     >
-      <header>{title}</header>
       <img src={`https://flagsapi.com/${params.countryCode}/flat/64.png`} />
-      <br />
-      Close button
       <br />
       Best global ranking: ?<br />
       Worst global ranking: ?<br />
       Graph of all results?
-      <p>Click away to close.</p>
-    </RMDrawer>
+    </AntDrawer>
   );
 }
 
